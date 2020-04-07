6998fa6a9b4b494b4fd5f7f86f91f141aad17957
Endo_Hizumi <endohizumi0114@gmail.com>
2020-04-07 16:01:08 +0900

    modify: remove unused import

--- stickyBoards_Server/main.py
+++ stickyBoards_Server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask
 from models.board import app as board_app
 from models.sticky import app as sticky_app
 from models.user import app as user_app
@@ -9,6 +9,7 @@ app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(board_app, url_prefix='/board')
 app.register_blueprint(sticky_app, url_prefix='/sticky')
 app.register_blueprint(user_app, url_prefix='/user')
+
 
 @app.route('/', methods={'GET'})
 def hello():

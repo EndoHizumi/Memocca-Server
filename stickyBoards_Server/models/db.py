6a0e62fce66abd07ed66313883b52310484091dc
Endo_Hizumi <endohizumi0114@gmail.com>
2020-04-16 00:58:19 +0900

    update: Unnecessary description

--- stickyBoards_Server/models/db.py
+++ stickyBoards_Server/models/db.py
@@ -2,11 +2,9 @@ import os
 import dataset
 
 
-class db:
+def get_connection():
+    user = os.getenv('STICKY_BOARDS_DB_ID')
+    password = os.getenv('STICKY_BOARDS_DB_PASS')
+    db_location = os.getenv('STICKY_BOARDS_DB_LOCATION')
 
-    def get_connection(self):
-        user = os.getenv['sticky_db_user']
-        password = os.getenv['sticky_db_password']
-        db_location = os.getenv['sticky_db_location']
-
-        return dataset.connect(f'mysql://{user}:{password}@{db_location}/board')
+    return dataset.connect(f'mysql://{user}:{password}@{db_location}/sticky_boards')

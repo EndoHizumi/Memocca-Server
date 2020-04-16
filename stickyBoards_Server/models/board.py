e1ddb61f87f3f195214508420367e8a57c534a70
Endo_Hizumi <endohizumi0114@gmail.com>
2020-04-16 12:42:13 +0900

    update: Unified way return of json

--- stickyBoards_Server/models/board.py
+++ stickyBoards_Server/models/board.py
@@ -9,14 +9,14 @@ sticky_db = db.get_connection()
 def get_all_boards():
     board_table = sticky_db['boards']
     result = board_table.find(private=0)
-    return jsonify([[row['board_id'], row['board_name']] for row in result])
+    return set_response_json(data)
 
 
 @app.route('/<board_id>', methods={'GET'})
 def get_board_info(board_id):
     board_table = sticky_db['boards']
     result = board_table.find(board_id=board_id)
-    return jsonify(list(result))
+    return set_response_json(list(result))
 
 
 @app.route('/<board_id>', methods={'POST'})
@@ -32,3 +32,12 @@ def update_board(board_id):
 @app.route('/<board_id>', methods={'DELETE'})
 def delete_board(board_id):
     return ("board.delete")
+
+def set_response_json(data, message=''):
+
+    if len(message) == 0:
+        responce = {'status': 'success'}
+    else:
+        responce = {'status': 'failed', 'message': message}
+    responce.update({'data': data})
+    return jsonify(responce)

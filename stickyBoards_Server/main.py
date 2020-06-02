ff84049d3628aa57fa75504636124e8ac18b2ca6
Endo_Hizumi <endohizumi0114@gmail.com>
2020-06-02 18:07:13 +0900

    modify: remove unused import

--- stickyBoards_Server/main.py
+++ stickyBoards_Server/main.py
@@ -1,12 +1,9 @@
-from flask import Flask, request, Response, stream_with_context
+from flask import Flask, request
 from flask_sse import sse
-from models.board import app as board_app
-from models.sticky import app as sticky_app
-from models.user import app as user_app
-from models import db, io_util
-from time import sleep
-from datetime import datetime
-import requests
+from .models.board import app as board_app
+from .models.sticky import app as sticky_app
+from .models.user import app as user_app
+from .models import db, io_util
 
 
 app = Flask(__name__)
@@ -46,25 +43,6 @@ def is_auth():
     skip_auth_page = request.path.split('/')[1] in ['', 'index.html', 'auth', '.png', '.jpg', '.gif', 'favicon.ico']
     if not is_access_token and not skip_auth_page:
         return io_util.set_response_json(data=None, message='not authorize', status=401)
-
-
-@app.route('/auth', methods={'POST'})
-def authorize():
-    request_json = request.json
-    room_id = request_json['room_id']
-    password = request_json['password']
-    name = request_json['name']
-    authToken = request_json['auth_token']
-
-    room_password = requests.get(f"http://127.0.0.1:8080/board/{room_id}").json()['password']
-    # リクエストのパスワードのハッシュ値を求めて、DBの値を比較する
-    if(room_password == password):
-        # cookieにAccessTokenをセット
-
-    else:
-        # passwordを再度要求する
-    app.logger.debug(room)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)

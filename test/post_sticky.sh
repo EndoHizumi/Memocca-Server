f6ad8f8db789e6f13ecc14419928a7f76c855123
Endo_Hizumi <endohizumi0114@gmail.com>
2020-06-14 22:26:14 +0900

    add: test_script

---
+++ test/post_sticky.sh
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:8080/sticky/0 -d '{"user_id":0, "board_id": "01234", "color_code":0, "text":"text", "point_x":0, "point_y":0, "width":0, "height":0}' -H "Content-Type: application/json"
